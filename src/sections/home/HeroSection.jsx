d8143a9fef3cb71c4c5c47861c5d9413896a44cb
Cami Wang <iris0952@gmail.com>
2026-01-24 16:38:31 +0800

    style: 修正排版

--- src/sections/home/HeroSection.jsx
+++ src/sections/home/HeroSection.jsx
@@ -2,10 +2,12 @@ function HeroSection() {
     return (
         <div className="heroSection">
             這是HERO區塊
-            <div className="main d-flex flex-row">
-                <div className="slogan">不知道去哪裡</div>
-                <div className="slogan-pic">
-                    <img src="https://cdn.wwads.cn/creatives/U3n343DCMimUvbTZX1xnbYkOoPlTBBVBjzP4bqjf.jpg" alt="" srcset="" />
+            <div className="container">
+                <div className="main d-flex flex-row">
+                    <h1 className="h1 slogan">不知道去哪裡？</h1>
+                    <div className="slogan-pic">
+                        <img src="https://cdn.wwads.cn/creatives/U3n343DCMimUvbTZX1xnbYkOoPlTBBVBjzP4bqjf.jpg" alt="" srcset="" />
+                    </div>
                 </div>
             </div>
             {/* <div className="big-cloud1"></div>
