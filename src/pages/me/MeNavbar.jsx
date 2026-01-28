2f77ccdf76ef446c75c526642b1e69efa5923914
UNO <wmei2004@gmail.com>
2026-01-28 20:45:54 +0800

    NAVBAR ROUTER 調整

---
+++ src/pages/me/MeNavbar.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom"
+
+
+function MeNavbar() {
+    return (
+        <div className="row">
+
+
+            <div className="d-grid gap-2">
+                <span>會員中心選單</span>
+                <button className="btn btn-primary" type="button">我的檔案</button>
+                <button className="btn btn-primary" type="button">我的旅程</button>
+                <button className="btn btn-primary" type="button">我的揪團</button>
+                <button className="btn btn-primary" type="button">我要開團</button>
+                <button className="btn btn-primary" type="button">我的收藏</button>
+                <button className="btn btn-primary" type="button">訊息通知</button>
+            </div>
+
+
+        </div>
+    )
+
+}
+export default MeNavbar
