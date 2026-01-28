49b886792be17b7b423d757608646f6fa569a0d1
Cami Wang <iris0952@gmail.com>
2026-01-28 21:35:55 +0800

    fix: 修正 navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,6 @@ function Navbar() {
             <div className="container">
                 {/* 1. Logo 區塊 */}
                 <a className="navbar-brand d-flex align-items-center" href="#">
-                    {/* 假設 logo 變數已定義，或直接放入 <img> */}
                     <img src={logo} alt="TripSeat" className="me-2" />
                 </a>
 
@@ -43,50 +42,4 @@ function Navbar() {
         </nav>
     );
 }
-// function Navbar() {
-
-//     return (
-//         <>
-//             <nav className="navbar container-fluid navbar-expand-lg navbar-light bg-light">
-//                 <div className="container trip-nav-inner">
-//                     <div className="trip-logo" ><img src={logo} alt="TripSeat" /></div>
-//                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//                         <span className="navbar-toggler-icon"></span>
-//                     </button>
-//                     <div className="collapse navbar-collapse" id="navbarNav">
-//                         {/* <div className="trip-nav-spacer"></div> */}
-//                         <div className="row">
-//                             <div class="col-md-3 offset-md-3">
-//                                 <div className=' trip-menu'>
-//                                     <ul className="navbar-nav mx-auto trip-menu">
-//                                         <li className="nav-item">
-//                                             <a className="nav-link btn link-l" href="#">探索旅程</a>
-//                                         </li>
-//                                         <li className="nav-item">
-//                                             <a className="nav-link btn link-l" href="#">回憶旅程</a>
-//                                         </li>
-//                                         <li className="nav-item">
-//                                             <a className="nav-link btn link-l" href="#">關於TripSeat</a>
-//                                         </li>
-
-//                                     </ul>
-
-//                                 </div>
-//                             </div>
-//                             <div class="col-md-3 offset-md-3">
-//                                 {/* <div className="trip-nav-right"> */}
-//                                 <button className="trip-btn btn trip-btn-primary trip-btn-m ms-auto">註冊 / ​登入</button>
-//                                 {/* </div> */}
-//                             </div>
-//                         </div>
-
-
-
-//                     </div>
-
-//                 </div>
-//             </nav>
-//         </>
-//     )
-// }
 export default Navbar
