c6eb6a4a29cff72e259cbbda09b2752d036546bf
UNO <wmei2004@gmail.com>
2026-01-29 01:26:01 +0800

    調整 navbar footer 新增 Demo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,35 +3,7 @@ function Footer() {
         <div className="footer">
             <p>2025© TripSeat | 共享旅程 All RIGHTS RESERVED.</p>
 
-            <p>
-                <h1 className="h1">h1 標題</h1>
-                <h2 className="h2">h2 標題</h2>
-                <h2 className="h3">h3 標題</h2>
-                <h2 className="h4">h4 標題</h2>
-                <h2 className="h5">h5 標題</h2>
-            </p>
-            <p>
-                <span className="trip-text-l">內文Ｌ、</span>
-                <span className="trip-text-m">內文M、</span>
-                <span className="trip-text-s">內文S</span> 
-            </p>
-            <p>
-                <a href="##" className="btn link-l">連結</a>
-                <a href="##" className="btn link-m">連結</a>
-                <a className="link-l disabled">連結</a>
 
-                <a href="##" className="btn link-m link-underline">查看更多</a>
-            </p>
-            <p>
-                <button className="btn trip-btn-primary trip-btn-l" type="button">我要開團</button>&nbsp;
-                <button className="btn trip-btn-primary trip-btn-m" type="button">我要開團</button>&nbsp;
-                <button className="btn trip-btn-primary trip-btn-s" type="button">我要開團</button>&nbsp;
-                <button className="btn trip-btn-primary trip-btn-s" type="button" disabled>我要開團</button>&nbsp;
-                <button className="btn trip-btn-outline-primary trip-btn-l" type="button">按鈕</button>&nbsp;
-                <button className="btn trip-btn-outline-primary trip-btn-m" type="button">按鈕</button>&nbsp;
-                <button className="btn trip-btn-outline-primary trip-btn-s" type="button">按鈕</button>&nbsp;
-                <button className="btn trip-btn-outline-primary trip-btn-s" type="button" disabled>按鈕</button>&nbsp;
-            </p>
         </div>
     )
 }
