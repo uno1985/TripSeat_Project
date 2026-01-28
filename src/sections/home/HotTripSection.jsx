c9d1859c8cb9c5ba4c4654fb3b369b5acb067f95
Cami Wang <iris0952@gmail.com>
2026-01-28 15:34:00 +0800

    fix: 熱門開團修改

--- src/sections/home/HotTripSection.jsx
+++ src/sections/home/HotTripSection.jsx
@@ -1,4 +1,6 @@
 import '../../assets/css/hotTripSection.css'
+import location from '../../assets/images/location.svg'
+import time from '../../assets/images/time.svg'
 import triprow01 from '../../assets/images/triprow01.png'
 import triprow02 from '../../assets/images/triprow02.png'
 import triprow03 from '../../assets/images/triprow03.png'
@@ -16,7 +18,7 @@ function HotTripSection() {
                             <div className="bg-white p-4 tripRow">
                                 <h2 className="h2 mb-4">熱門開團</h2>
                                 {/* 第一筆資料 */}  
-                                <div className="d-flex py-3 position-relative">
+                                <div className="d-flex pt-3 position-relative">
                                     {/* 圖片區塊 */}
                                     <div className="flex-shrink-0">
                                         <div className="tripImg">
@@ -28,29 +30,30 @@ function HotTripSection() {
                                     <div className="flex-grow-1 ms-3">
                                         <div className="d-flex justify-content-between align-items-start">
                                             <h4 className="h4 mb-2">2026 跨年 101煙火團</h4>
-                                            <span className="trip-text-m trip-text-gray-400">136人瀏覽</span>
+                                            <span className="trip-text-m trip-text-gray-400 desktop-show">136人瀏覽</span>
                                         </div>
                                         
                                         <div className="mb-1">
                                             <p className="trip-text-m mb-1 d-flex align-items-center">
-                                                <img src="public/assets/location.svg" className="me-2" alt="" />
+                                                <img src={location} className="me-2 icon-location" alt="" />
                                                 <span>台北 101 大樓 1F</span>
                                             </p>
                                             <p className="trip-text-m mb-0">
-                                                <img src="public/assets/time.svg" className="me-2" alt="" />
+                                                <img src={time} className="me-2" alt="" />
                                                 <span>2025/12/31 23:00</span>
                                             </p>
                                         </div>
 
                                         {/* 剩餘位數標籤 (絕對定位在右下角) */}
-                                        <div className="lastSeat position-absolute bottom-0 end-0 px-2 py-1 trip-text-m">
-                                            剩餘 1 位
+                                        <div className="d-flex align-items-center justify-content-end flex-row px-2 py-1">
+                                            <span className="trip-text-m trip-text-gray-400 me-3 mobile-show">136人瀏覽</span>
+                                            <span className='lastSeat trip-text-m px-2 py-1'>剩餘 1 位</span>
                                         </div>
                                     </div>
                                 </div>
                                 <hr />
                                 {/* 第二筆資料 */}
-                                <div className="d-flex py-3 position-relative">
+                                <div className="d-flex pt-3 position-relative">
                                     {/* 圖片區塊 */}
                                     <div className="flex-shrink-0">
                                         <div className="tripImg">
@@ -62,30 +65,31 @@ function HotTripSection() {
                                     <div className="flex-grow-1 ms-3">
                                         <div className="d-flex justify-content-between align-items-start">
                                             <h4 className="h4 mb-2">彩妝課程五人優惠團</h4>
-                                            <span className="trip-text-m trip-text-gray-400">36人瀏覽</span>
+                                            <span className="trip-text-m trip-text-gray-400 desktop-show">36人瀏覽</span>
                                         </div>
                                         
                                         <div className="mb-1">
                                             <p className="trip-text-m mb-1 d-flex align-items-center">
-                                                <img src="public/assets/location.svg" className="me-2" alt="" />
+                                                <img src={location} className="me-2 icon-location" alt="" />
                                                 <span>台中大遠百門口集合</span>
                                             </p>
                                             <p className="trip-text-m mb-0">
-                                                <img src="public/assets/time.svg" className="me-2" alt="" />
+                                                <img src={time} className="me-2" alt="" />
                                                 <span>2026/03/04 23:00</span>
                                             </p>
                                         </div>
 
                                         {/* 剩餘位數標籤 (絕對定位在右下角) */}
-                                        <div className="lastSeat position-absolute bottom-0 end-0 px-2 py-1 trip-text-m">
-                                            剩餘 1 位
+                                        <div className="d-flex align-items-center justify-content-end flex-row px-2 py-1">
+                                            <span className="trip-text-m trip-text-gray-400 me-3 mobile-show">36人瀏覽</span>
+                                            <span className='lastSeat trip-text-m px-2 py-1'>剩餘 1 位</span>
                                         </div>
                                     </div>
                                 </div>
                                 <hr />
 
                                 {/* 第三筆資料 */}
-                                <div className="d-flex py-3 position-relative">
+                                <div className="d-flex pt-3 position-relative">
                                     {/* 圖片區塊 */}
                                     <div className="flex-shrink-0">
                                         <div className="tripImg">
@@ -97,23 +101,24 @@ function HotTripSection() {
                                     <div className="flex-grow-1 ms-3">
                                         <div className="d-flex justify-content-between align-items-start">
                                             <h4 className="h4 mb-2">台南山上聖誕市集共乘</h4>
-                                            <span className="trip-text-m trip-text-gray-400">236人瀏覽</span>
+                                            <span className="trip-text-m trip-text-gray-400 desktop-show">236人瀏覽</span>
                                         </div>
                                         
                                         <div className="mb-1">
                                             <p className="trip-text-m mb-1 d-flex align-items-center">
-                                                <img src="public/assets/location.svg" className="me-2" alt="" />
+                                                <img src={location} className="me-2 icon-location" alt="" />
                                                 <span>台南仁德家樂福集合</span>
                                             </p>
                                             <p className="trip-text-m mb-0">
-                                                <img src="public/assets/time.svg" className="me-2" alt="" />
+                                                <img src={time} className="me-2" alt="" />
                                                 <span>2025/12/20 23:00</span>
                                             </p>
                                         </div>
 
                                         {/* 剩餘位數標籤 (絕對定位在右下角) */}
-                                        <div className="lastSeat position-absolute bottom-0 end-0 px-2 py-1 trip-text-m">
-                                            剩餘 1 位
+                                        <div className="d-flex align-items-center justify-content-end flex-row px-2 py-1">
+                                            <span className="trip-text-m trip-text-gray-400 me-3 mobile-show">336人瀏覽</span>
+                                            <span className='lastSeat trip-text-m px-2 py-1'>剩餘 1 位</span>
                                         </div>
                                     </div>
                                 </div>
@@ -125,7 +130,7 @@ function HotTripSection() {
                             <div className="bg-white p-4 tripRow">
                                 <h2 className="h2 mb-4">最新開團</h2>
                                 {/* 第一筆資料 */}  
-                                <div className="d-flex py-3 position-relative">
+                                <div className="d-flex pt-3 position-relative">
                                     {/* 圖片區塊 */}
                                     <div className="flex-shrink-0">
                                         <div className="tripImg">
@@ -137,29 +142,30 @@ function HotTripSection() {
                                     <div className="flex-grow-1 ms-3">
                                         <div className="d-flex justify-content-between align-items-start">
                                             <h4 className="h4 mb-2">手工皮件課程三人成團</h4>
-                                            <span className="trip-text-m trip-text-gray-400">12/12 開團</span>
+                                            <span className="trip-text-m trip-text-gray-400 desktop-show">12/12 開團</span>
                                         </div>
                                         
                                         <div className="mb-1">
                                             <p className="trip-text-m mb-1 d-flex align-items-center">
-                                                <img src="public/assets/location.svg" className="me-2" alt="" />
+                                                <img src={location} className="me-2 icon-location" alt="" />
                                                 <span>高雄漢神巨蛋一樓集合</span>
                                             </p>
                                             <p className="trip-text-m mb-0">
-                                                <img src="public/assets/time.svg" className="me-2" alt="" />
+                                                <img src={time} className="me-2" alt="" />
                                                 <span>2025/12/12 13:00</span>
                                             </p>
                                         </div>
 
                                         {/* 剩餘位數標籤 (絕對定位在右下角) */}
-                                        <div className="lastSeat position-absolute bottom-0 end-0 px-2 py-1 trip-text-m">
-                                            剩餘 1 位
+                                        <div className="d-flex align-items-center justify-content-end flex-row px-2 py-1">
+                                            <span className="trip-text-m trip-text-gray-400 me-3 mobile-show">12/12 開團</span>
+                                            <span className='lastSeat trip-text-m px-2 py-1'>剩餘 1 位</span>
                                         </div>
                                     </div>
                                 </div>
                                 <hr />
                                 {/* 第二筆資料 */}
-                                <div className="d-flex py-3 position-relative">
+                                <div className="d-flex pt-3 position-relative">
                                     {/* 圖片區塊 */}
                                     <div className="flex-shrink-0">
                                         <div className="tripImg">
@@ -171,30 +177,31 @@ function HotTripSection() {
                                     <div className="flex-grow-1 ms-3">
                                         <div className="d-flex justify-content-between align-items-start">
                                             <h4 className="h4 mb-2">阿里山小火車一日遊共乘</h4>
-                                            <span className="trip-text-m trip-text-gray-400">12/08 開團</span>
+                                            <span className="trip-text-m trip-text-gray-400 desktop-show">12/08 開團</span>
                                         </div>
                                         
                                         <div className="mb-1">
                                             <p className="trip-text-m mb-1 d-flex align-items-center">
-                                                <img src="public/assets/location.svg" className="me-2" alt="" />
+                                                <img src={location} className="me-2 icon-location" alt="" />
                                                 <span>嘉義火車站集合</span>
                                             </p>
                                             <p className="trip-text-m mb-0">
-                                                <img src="public/assets/time.svg" className="me-2" alt="" />
+                                                <img src={time} className="me-2" alt="" />
                                                 <span>2025/12/19 08:00</span>
                                             </p>
                                         </div>
 
                                         {/* 剩餘位數標籤 (絕對定位在右下角) */}
-                                        <div className="lastSeat position-absolute bottom-0 end-0 px-2 py-1 trip-text-m">
-                                            剩餘 1 位
+                                        <div className="d-flex align-items-center justify-content-end flex-row px-2 py-1">
+                                            <span className="trip-text-m trip-text-gray-400 me-3 mobile-show">12/08 開團</span>
+                                            <span className='lastSeat trip-text-m px-2 py-1'>剩餘 1 位</span>
                                         </div>
                                     </div>
                                 </div>
                                 <hr />
 
                                 {/* 第三筆資料 */}
-                                <div className="d-flex py-3 position-relative">
+                                <div className="d-flex pt-3 position-relative">
                                     {/* 圖片區塊 */}
                                     <div className="flex-shrink-0">
                                         <div className="tripImg">
@@ -206,23 +213,24 @@ function HotTripSection() {
                                     <div className="flex-grow-1 ms-3">
                                         <div className="d-flex justify-content-between align-items-start">
                                             <h4 className="h4 mb-2">卓也藍染、日月潭一日遊</h4>
-                                            <span className="trip-text-m trip-text-gray-400">12/07 開團</span>
+                                            <span className="trip-text-m trip-text-gray-400 desktop-show">12/07 開團</span>
                                         </div>
                                         
                                         <div className="mb-1">
                                             <p className="trip-text-m mb-1 d-flex align-items-center">
-                                                <img src="public/assets/location.svg" className="me-2" alt="" />
+                                                <img src={location} className="me-2 icon-location" alt="" />
                                                 <span>苗栗火車站門口集合</span>
                                             </p>
                                             <p className="trip-text-m mb-0">
-                                                <img src="public/assets/time.svg" className="me-2" alt="" />
+                                                <img src={time} className="me-2" alt="" />
                                                 <span>2025/12/17 07:30</span>
                                             </p>
                                         </div>
 
                                         {/* 剩餘位數標籤 (絕對定位在右下角) */}
-                                        <div className="lastSeat position-absolute bottom-0 end-0 px-2 py-1 trip-text-m">
-                                            剩餘 1 位
+                                        <div className="d-flex align-items-center justify-content-end flex-row px-2 py-1">
+                                            <span className="trip-text-m trip-text-gray-400 me-3 mobile-show">12/07 開團</span>
+                                            <span className='lastSeat trip-text-m px-2 py-1'>剩餘 1 位</span>
                                         </div>
                                     </div>
                                 </div>
